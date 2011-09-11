2f8acb6bd81eb9360eb1c09b72be90891d2dd86a
Hidden <zzdhidden@gmail.com>
2011-09-11 15:14:17 +0800

    Init

---
+++ test/test_helper.js
@@ -0,0 +1,37 @@
+//exports = module.exports = global;
+QUnit = require("./qunit/qunit.js").QUnit;
+var qunitTap = require("./qunit/qunit-tap.js").qunitTap;
+jQuery = require( "../index.js" );
+
+var sys = tryRequireThese("sys", "system");
+puts = (typeof sys.puts === 'function') ? sys.puts : sys.print;
+
+qunitTap(QUnit, puts, {noPlan: true});
+
+QUnit.init();
+QUnit.config.blocking = false;
+QUnit.config.autorun = true;
+QUnit.config.updateRate = 0;
+QUnit.tap.showDetailsOnFailure = true;
+
+assert = QUnit;
+extend(global, QUnit);
+
+function extend(a, b) {
+	for ( var prop in b ) {
+		a[prop] = b[prop];
+	}
+	return a;
+}
+
+function tryRequireThese() {
+	var args = Array.prototype.slice.apply(arguments);
+	for(var i=0; i < args.length; i+=1) {
+		try {
+			return require(args[i]);
+		} catch(e) {
+			// ignore
+		}
+	}
+	throw new Error("cannot find moduele: " + args);
+}
